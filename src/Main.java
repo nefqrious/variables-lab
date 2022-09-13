981c0bf1bcafe9b277b15ddddeb5dea8fad6e5fc
nefqrious <sonnyelijahalbano@gmail.com>
2022-09-13 09:56:29 -0500

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,33 @@
+public class Main
+{
+    public static void main ( String[] args )
+    {
+        //define 1 variable of each of the
+        //following data types
+        //byte	short		int 		long
+        //float	double
+        //char      boolean	String
+
+        //integer variables
+        byte x = 127;
+        short y = 128;
+        int z = 129;
+        long u = 130L;
+        float e = 5.11F;
+        double n = 5.12;
+        char d = '$';
+        boolean t = true;
+        String i = "water";
+
+
+        System.out.println(x);
+        System.out.println(y);
+        System.out.println(z);
+        System.out.println(u);
+        System.out.println(e);
+        System.out.println(n);
+        System.out.println(d);
+        System.out.println(t);
+        System.out.println(i);
+    }
+}
